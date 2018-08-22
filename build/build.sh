0292855c622d226f40db575afdd4b2f61ca56329
trimstray <trimstray@gmail.com>
2018-08-22 22:45:37 +0200

    removed shellcheck comments, make log directory in project - signed-off-by: trimstray <trimstray@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck shell=bash
-# shellcheck disable=SC1091,SC2034
+# shellcheck disable=SC1091,SC2016,SC2034
 
 source metadata
 
@@ -10,34 +10,28 @@ cd ../project || exit 1
 mv bin/__NAME__ bin/"$__NAME__"
 mv doc/man8/__NAME__.8 doc/man8/"$__NAME__".8
 
-# shellcheck disable=SC2016
+mkdir log
+
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__DATE__/$__DATE__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__NAME__/$__NAME__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__AUTHOR__/$__AUTHOR__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__EMAIL__/$__EMAIL__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s~__REPO__~$__REPO__~g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__VERSION__/$__VERSION__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__SHORT_DESCRIPTION__/$__SHORT_DESCRIPTION__/g"
 
-# shellcheck disable=SC2016
 find . -type f -not -path "./build/*" -print0 | \
 xargs -0 sed -i "s/__LONG_DESCRIPTION__/$__LONG_DESCRIPTION__/g"
