c20796dc4a549c388b0e0ee3f277c83fc5d08ca6
trimstray <trimstray@gmail.com>
2018-08-22 20:24:11 +0200

    updated build.sh, minor fixes - signed-off-by: trimstray <trimstray@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -5,34 +5,37 @@ source metadata
 
 cd ../project || exit 1
 
-# shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__DATE__/$__DATE__/g" {} \;
+mv bin/__NAME__ bin/"$__NAME__"
+mv doc/man8/__NAME__.8 doc/man8/"$__NAME__".8
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__NAME__/$__NAME__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__DATE__/$__DATE__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__AUTHOR__/$__AUTHOR__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__NAME__/$__NAME__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__EMAIL__/$__EMAIL__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__AUTHOR__/$__AUTHOR__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__REPO__/$__REPO__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__EMAIL__/$__EMAIL__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__VERSION__/$__VERSION__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__REPO__/$__REPO__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__SHORT_DESCRIPTION__/$__SHORT_DESCRIPTION__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__VERSION__/$__VERSION__/g"
 
 # shellcheck disable=SC2016
-find . -type f -not -path "./build/*" \
-       -exec sed -i "s/__LONG_DESCRIPTION__/$__LONG_DESCRIPTION__/g" {} \;
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__SHORT_DESCRIPTION__/$__SHORT_DESCRIPTION__/g"
+
+# shellcheck disable=SC2016
+find . -type f -not -path "./build/*" -print0 | \
+xargs -0 sed -i "s/__LONG_DESCRIPTION__/$__LONG_DESCRIPTION__/g"
